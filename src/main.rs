68e5c9cf7af6ff58ab7e757a19167c8f7b2785c4
Joris Valette <joris.valette@gmail.com>
2018-03-11 17:55:20 +0100

    Detect VM116

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate libusb;
+
 fn main() {
-    println!("Hello, world!");
+    let context = libusb::Context::new().unwrap();
+    let mut handle = None;
+    println!("Browsing USB devices to find a VM116...");
+    for mut device in context.devices().unwrap().iter() {
+        let device_desc = device.device_descriptor().unwrap();
+        // VM116 device id is 10cf:8062
+        if device_desc.vendor_id() == 0x10cf && device_desc.product_id() == 0x8062 {
+            handle = match device.open() {
+                Ok(handle) => Some(handle),
+                Err(_) => {
+                    println!("VM116 found but could not open it. :(");
+                    return;
+                }
+            }
+        }
+    }
+    match handle {
+        Some(_) => println!("VM116 found! :)"),
+        None => println!("VM116 not found. :(")
+    }
 }
