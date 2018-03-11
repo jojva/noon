6eaff149540603b935a46ab4c6186c9c210cdc32
Joris Valette <joris.valette@gmail.com>
2018-03-11 18:47:50 +0100

    Add all USB devices detection

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ fn main() {
     println!("Browsing USB devices to find a VM116...");
     for mut device in context.devices().unwrap().iter() {
         let device_desc = device.device_descriptor().unwrap();
+
+        println!("Bus {:03} Device {:03} ID {:04x}:{:04x}",
+            device.bus_number(),
+            device.address(),
+            device_desc.vendor_id(),
+            device_desc.product_id());
+
         // VM116 device id is 10cf:8062
         if device_desc.vendor_id() == 0x10cf && device_desc.product_id() == 0x8062 {
             handle = match device.open() {
